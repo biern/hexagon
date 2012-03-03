bf6a85eac25b119274104d199a1c0691dbf9b249
biern <biern.m@gmail.com>
2012-03-03 23:16:46 +0100

    added console to tests

--- client/scripts/tests.js
+++ client/scripts/tests.js
@@ -1,5 +1,10 @@
 
-YUI(YUI_config).use('hexagon.widgets.board.tests', 'hexagon.models.plugs.boardstate.tests', function(Y){
+YUI(YUI_config).use('hexagon.widgets.board.tests', 'hexagon.models.plugs.boardstate.tests', 'console', function(Y){
+
+    new Y.Console({
+        width: '400px',
+        height: '700px'
+    }).render('#console');
 
     Y.Test.Runner.add(Y.Hexagon.widgets.board.tests.suite);
     Y.Test.Runner.add(Y.Hexagon.models.plugs.boardstate.tests.suite);
