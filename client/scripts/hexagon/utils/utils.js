e6bdbed6a3431e56422191907a7096246ed1c593
biern <biern.m@gmail.com>
2012-03-03 22:56:49 +0100

    added utils.assertObjectParams

--- client/scripts/hexagon/utils/utils.js
+++ client/scripts/hexagon/utils/utils.js
@@ -1,5 +1,7 @@
 YUI.add('hexagon.utils', function (Y) {
 
+    // TODO: Think about separating this module
+
     var namespace = Y.namespace('Hexagon.utils');
 
     namespace.assertFired = function (func, obj, name, handler) {
@@ -14,6 +16,15 @@ YUI.add('hexagon.utils', function (Y) {
         func();
         sub.detach();
         Y.Assert.isTrue(fired, 'Event ' + name + ' did not fire');
+    };
+
+    /**
+     * Asserts that object contains all params (contains keys with equal values)
+     */
+    namespace.assertObjectParams = function (obj, params) {
+        for (k in params) {
+            Y.Assert.areSame(obj[k], params[k]);
+        }
     };
 
 }, '0', {
