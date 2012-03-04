bd4193f18def2d2bbc77d679648d22a57992fb3c
biern <biern.m@gmail.com>
2012-03-04 12:39:07 +0100

    bind Y to window

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,7 +1,7 @@
 
-YUI(YUI_config).use('hexagon.models.game', 'hexagon.views.game',
-                    'hexagon.server.testserver', function(Y){
+YUI(YUI_config).use('hexagon.models.game', 'hexagon.views.game', 'hexagon.server.testserver', function(Y){
 
+    window.Y = Y;
     window.server = new Y.Hexagon.server.Test();
 
     var model = new Y.Hexagon.models.Game({
