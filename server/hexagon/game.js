c482d4d2e7ddeee70a8009dfd75b5345ed84cd76
Marcin Biernat <mb@marcinbiernat.pl>
2012-09-18 22:17:34 +0200

    server game big update

--- server/hexagon/game.js
+++ server/hexagon/game.js
@@ -1,61 +1,112 @@
 var socket_io = require('socket.io'),
     yvents = require('yvents'),
     logic = require('./logic'),
+    hexutils = require('./utils'),
     maps = require('./maps');
 
 
 function Game (bus, id) {
     yvents.Base.call(this, bus);
-    this.games = [];
+    this.boards = [];
 };
 
 
 yvents.subclass(Game, { prefix: 'game' }, {
     EVENTS: {
-        after: ['game:create']
+        after: ['game:create', 'player.board:join']
     },
 
     _afterGameCreate: function (e) {
-        var id = this.games.length;
-        console.log('game create', id);
-        this.games.push(new HexagonGame(
+        var id = this.boards.length  + 1;
+
+        this.boards.push(new Board(
             id, maps[0], e.players));
+    },
+
+    _afterPlayerBoardJoin: function (e) {
+        console.log(e);
+        var id = e.boardID,
+            found = false;
+
+        this.boards.every(function (board) {
+            if (board.id == id) {
+                board.joinPlayer(e.player);
+                found = true;
+                return false;
+            }
+            return true;
+        });
+
+        if (!found) {
+            // TODO e.socket
+            e.player.socket.emit('board:invalid', { baordID: id });
+        }
     }
 });
 
 
-function HexagonGame (id, map, players) {
+function Board (id, map, players) {
     this.id = id;
-    this.player1 = players[0];
-    this.player2 = players[1];
+
+    // Limited to currently connected players
     this._players = players;
+
+    // Names of players that take part in the game
+    // this does not change on leave / join
+    this._playersNames = this._players.map(function (p) { return p.username; });
+
     this.map = map;
+
+    this._initState();
 
     this.players(function (p) {
         this._bindPlayer(p);
     });
 
     this.players(function (p) {
-        p.socket.emit('game:join', { id: id });
+        p.socket.emit('board:join', { id: id });
     });
+
 };
 
-HexagonGame.prototype = {
+Board.prototype = {
+
+    _initState: function () {
+        // Hack hack hack - deep clone
+        this.state = JSON.parse(JSON.stringify(this.map.board));
+        this.state.activePlayerID = this._players[0].username;
+        this.state.allPlayers = this._playersNames;
+        this.state.playersStyles = this._players.map(
+            function (p) { return p.style; });
+
+        hexutils.fixStyles(this.state.playersStyles);
+    },
 
     players: function (callback) {
         var args = [],
             res = [];
 
-        args[0] = this.player1;
+        args[0] = this._players[0];
         [].push.apply(args, Array.prototype.slice.call(arguments, 1));
         res.push(callback.apply(this, args));
-        args[0] = this.player2;
+        args[0] = this._players[1];
         res.push(callback.apply(this, args));
         return res;
     },
 
-    _bindPlayer: function (player) {
+    joinPlayer: function (player) {
+        if (this._playersNames.indexOf(player.username) != -1) {
+            console.log('player', player.username, 'returned');
+            this._bindPlayer(player);
+        }
+    },
 
+    _bindPlayer: function (player) {
+        player.after('board:resync', this._afterPlayerBoardResync, this);
+    },
+
+    _afterPlayerBoardResync: function (e) {
+        e.socket.emit('board:state', this.state);
     },
 
     toJSON: function () {
